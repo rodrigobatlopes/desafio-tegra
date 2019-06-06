ea4056b77b68f38106834d4e1979469962190f92
Rodrigo Lopes <rodrigo.bat.lopes@gmail.com>
2019-06-05 21:44:10 -0300

    added swagger configs

--- desafioTegra/src/main/java/br/com/rodrigolopes/controller/AirportRestController.java
+++ desafioTegra/src/main/java/br/com/rodrigolopes/controller/AirportRestController.java
@@ -1,12 +1,13 @@
 package br.com.rodrigolopes.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.HttpEntity;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.support.GenericMessage;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,17 +17,22 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.rodrigolopes.desafioTegra.domain.Aeroporto;
 import br.com.rodrigolopes.desafioTegra.service.AirportService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 
 
 @RestController
 @RequestMapping("/airports")
 @Slf4j
+@CrossOrigin(origins = "*")
+@Api(value="API REST Aeroportos")
 public class AirportRestController {
 
 	@Autowired
 	public AirportService airportService;
 	
+	@ApiOperation(value="Retorna uma lista de Aeroportos")
     @GetMapping(value={"/list"},
             produces = {MediaType.APPLICATION_JSON_UTF8_VALUE})
     public ResponseEntity<Object> getAirports() {
@@ -40,6 +46,7 @@ public class AirportRestController {
     
     MessageChannel providerMessageChannel;
     
+	@ApiOperation(value="Cadastra um novo Aeroporto")
     @PostMapping(value = "/new",
             produces = {MediaType.APPLICATION_JSON_UTF8_VALUE},
             consumes = {MediaType.APPLICATION_JSON_UTF8_VALUE}
